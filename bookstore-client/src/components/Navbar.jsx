72001b21f663b4642c001b45e4a95b7b80623669
vibey19 <jdadhaniya6@gmail.com>
2024-02-22 19:22:56 +0530

    designed navbar comp

--- bookstore-client/src/components/Navbar.jsx
+++ bookstore-client/src/components/Navbar.jsx
@@ -1,7 +1,108 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { GiSpellBook } from "react-icons/gi";
+import { FaBarsStaggered, FaXmark } from "react-icons/fa6";
 const Navbar = () => {
-  return <div>this is a navbar</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
+
+  // Toggle Menu
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.addEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // Navbar Items
+  const navItems = [
+    { link: "Home", path: "/" },
+    { link: "About", path: "/about" },
+    { link: "Shop", path: "/shop" },
+    { link: "Sell Your Book", path: "/admin/dashboard" },
+    { link: "Blog", path: "/blog" },
+  ];
+
+  return (
+    <header className="w-full bg-transparent fixed top-0 right-0 translate-all ease-in duration-300">
+      <nav
+        className={`py-4 lg:px-24 px-4 ${
+          isSticky ? "sticky top-0 left-0 right-0 bg-blue-300" : ""
+        }`}
+      >
+        <div className="flex justify-between items-center text-base gap-8">
+          {/* LOGO */}
+          <Link
+            to="/"
+            className="text-2xl font-bold text-blue-700 flex items-center gap-2"
+          >
+            <GiSpellBook className="inline-block" />
+            BookBard
+          </Link>
+
+          {/* NAV ITEMS FOR LARGE SCREEN */}
+          <ul className="md:flex space-x-12 hidden">
+            {navItems.map(({ link, path }) => (
+              <Link
+                key={path}
+                to={path}
+                className="block text-base text-black cursor-pointer hover:text-blue-700"
+              >
+                {link}
+              </Link>
+            ))}
+          </ul>
+
+          {/* BUTTON FOR LARGE SCREEN */}
+          <div className="hidden space-x-12 lg:flex items-center">
+            <button>
+              <FaBarsStaggered className="w-5 hover:text-blue-700" />
+            </button>
+          </div>
+
+          {/* MENU BUTTON FOR MOBILE SCREEN */}
+          <div className="md:hidden">
+            <button onClick={toggleMenu} className="focus:outline-none">
+              {isMenuOpen ? (
+                <FaXmark className="h-5 w-5 text-black" />
+              ) : (
+                <FaBarsStaggered className="h-5 w-5 text-black" />
+              )}
+            </button>
+          </div>
+
+          {/* NAV ITEMS FOR MOBILE SCREEN */}
+          <div
+            className={`space-y-4 px-4 mt-16 bg-blue-700 ${
+              isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"
+            }`}
+          >
+            {navItems.map(({ link, path }) => (
+              <Link
+                key={path}
+                to={path}
+                className="block text-base text-white cursor-pointer "
+              >
+                {link}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
