83748f840abd5345738c7b2ba108353b609303c2
vibey19 <jdadhaniya6@gmail.com>
2024-02-25 11:40:40 +0530

    add footer component

--- bookstore-client/src/App.jsx
+++ bookstore-client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import FooterElement from "./components/FooterElement";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
     <>
       <Navbar />
       <Outlet />
+      <FooterElement />
     </>
   );
 }
